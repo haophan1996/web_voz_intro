72da7517cfe30c909502668b9704c523e798690f
Hao Phan <71293555+haophan1996@users.noreply.github.com>
2025-03-31 18:46:04 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,89 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMenu);
+
+
+window.onload = function () { 
+  const savedTheme = localStorage.getItem("theme") || "light";
+  if (savedTheme === "light"){ 
+    setMoonIcon()
+  } else {
+    setlightIcon()
+  }
+
+};
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link"); 
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Event Listeners: Handling toggle event
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) { 
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark"); 
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");  
+  }
+}
+
+
+function setlightIcon(){
+  document.querySelector(".sun").style.opacity = "1"; 
+  document.querySelector(".moon").style.opacity = "0";  
+}
+
+function setMoonIcon(){
+  document.querySelector(".sun").style.opacity = "0"; 
+  document.querySelector(".moon").style.opacity = "1";  
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//  Store color theme for future visits
+
+function switchTheme(e) {
+  if (e.target.checked) { 
+    setlightIcon()
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this 
+  } else { 
+    setMoonIcon()
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this 
+  }
+}
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+//Adding date
+
+let myDate = document.querySelector("#datee");
+
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
